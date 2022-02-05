91c36b89dd528d8b7855c9a7e5f15dc48ad89138
hincza <34672780+h1ncza@users.noreply.github.com>
2022-02-05 18:54:08 +0100

    first commit

---
+++ go-http-client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	URL string
+)
+
+func init() {
+	flag.StringVar(&URL, "h", "", "URL to connect to")
+}
+
+func main() {
+	flag.Parse()
+
+	if len(URL) == 0 {
+		fmt.Println("Usage: http-goclient -h <HOST>")
+		// flag.PrintDefaults()
+		os.Exit(1)
+	} else if strings.HasPrefix(URL, "http://") || strings.HasPrefix(URL, "https://") {
+		URL = URL
+	} else {
+		URL = "http://" + URL
+	}
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("Connection refused")
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response status:", resp.Status)
+	respByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(respByte))
+}
